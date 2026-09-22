47c8a1003f1042e3518cf8e1bbb27fcc43ae28a7
eng1986y-cyber <eng1986y@gmail.com>
2026-09-22 19:44:49 +0900

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "https://fwhhtpxaqsciyiocpqrz.supabase.co";
+const supabaseKey = "sb_publishable_HtNaob6tU7s-BhzNigTQBA_CtSbvGaB";
+
+const supabaseClient = supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
